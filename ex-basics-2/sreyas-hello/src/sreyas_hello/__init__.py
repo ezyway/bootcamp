3f0bda35ffe30a584282be2976c08394d6d4fd83
Sreyas <sreyas.codaw@gmail.com>
2025-08-20 14:18:47 +0530

    adds more examples

--- ex-basics-2/sreyas-hello/src/sreyas_hello/__init__.py
+++ ex-basics-2/sreyas-hello/src/sreyas_hello/__init__.py
@@ -7,5 +7,9 @@ def main() -> None:
     who = sys.argv[1] if len(sys.argv) > 1 else "world"
     
     console.print(f"Hello, [bold red]{who}[/bold red]")
-    # OR
     console.print(f"Hello, {who}", style='underline blue')
+    console.print("Hello [blue]Alice[/blue]") 
+    console.print("Hello [bold green]Alice[/]")      # Bold green ([/] closes all tags)
+    console.print("Hello [white on red]Alice[/]")    # White on red background
+    console.print("Hello [bold italic cyan]Alice[/]")
+
